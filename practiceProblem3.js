4f47c7cb5fa15e8b8e024da5edd437d41d1c7a6f
mahiralshahiar <mrmahiralshariar9@gmail.com>
2023-07-31 02:13:24 +0600

    practiceProblem3Solved

---
+++ practiceProblem3.js
@@ -0,0 +1,7 @@
+// John teacher 2 ti variable diyeche single line e space diye show korte hobe 
+
+var firstLine = 'I am goint to be'
+var secondLine = 'an awesome web developer'
+
+var title = firstLine + ' ' + secondLine
+console.log(title)
