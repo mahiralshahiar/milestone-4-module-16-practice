fa78436fac772f43668bf9049f3917738fc41388
mahiralshahiar <mrmahiralshariar9@gmail.com>
2023-07-31 01:51:41 +0600

    practiceProblem1Solved

---
+++ practiceProblem1.js
@@ -0,0 +1,17 @@
+// Practice Problem 1
+// Harrys topic
+
+
+// Harry's mom gave him total 1000 Taka.
+
+var totalGivenMoney = 1000;
+console.log(totalGivenMoney)
+// Orange price
+
+var applePrice = 400;
+var orangePrice = 300;
+var totalSpend = applePrice + orangePrice;
+console.log(totalSpend)
+
+var returnedMoneyByTheShopKeeper = totalGivenMoney - totalSpend;
+console.log(returnedMoneyByTheShopKeeper)
